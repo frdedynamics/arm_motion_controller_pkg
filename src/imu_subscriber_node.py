d25ad0c28dc58044c36a4537430ae00dd84815bc
gizem <gizemates@iyte.edu.tr>
2021-04-16 14:59:55 +0200

    2dof elbox axis and everything ok

---
+++ src/imu_subscriber_node.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import rospy
+from Classes.imu_subscriber_class import IMUsubscriber
+from geometry_msgs.msg import Vector3
+
+if __name__ == "__main__": 
+	IMU = IMUsubscriber(rate=100, start_node=True)
+	IMU.init_subscribers_and_publishers()
+	while not rospy.is_shutdown():
+		IMU.update()
+		IMU.r.sleep()
