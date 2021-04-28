a6d38f8f68e45fb929d4732a0d65c4c7668b4c17
gizem <gizemates@iyte.edu.tr>
2021-04-28 22:02:30 +0200

    backup for old moveit robot_move

--- src/robot_move.py
+++ src/robot_move.py
@@ -37,48 +37,54 @@ def movegroup_init():
 	plan_arm = arm_group.go()  
 	return arm_group
 
-def plan_task_space_control(arm_group, robot_init, hand_pose):
+def cartesian_control_with_IMU(arm_group, robot_init, hand_pose, *argv):
 	waypoints = []
-	scale = 1.0
+	scale = 3.0
 	
-	tpose = Pose()
-	tpose.position.x = robot_init.position.x + scale * hand_pose.position.y
-	tpose.position.y = robot_init.position.y + scale * hand_pose.position.z
-	tpose.position.z = robot_init.position.z + scale * hand_pose.position.x
-	tpose.orientation = robot_init.orientation
+	wpose = Pose()
+	wpose.position.x = robot_init.position.x + scale * hand_pose.position.y
+	wpose.position.y = robot_init.position.y + scale * hand_pose.position.x
+	wpose.position.z = robot_init.position.z + scale * hand_pose.position.z
+	wpose.orientation = robot_init.orientation
+	# wpose.orientation = kinematic.q_multiply(robot_init.orientation, hand_pose.orientation)
 	
-	waypoints.append(copy.deepcopy(tpose))
+	waypoints.append(copy.deepcopy(wpose))
 	
 	(plan, fraction) = arm_group.compute_cartesian_path(
                                    waypoints,   # waypoints to follow
-                                   0.01,        # eef_step
+                                   0.1,        # eef_step
                                    0.0)         # jump_threshold
 
-	# arm_group.execute(plan, wait=True)
-	# arm_group.stop()
-	# arm_group.clear_pose_targets()
-
-	return tpose
+	arm_group.execute(plan, wait=False)
+	arm_group.stop()
+	arm_group.clear_pose_targets()
 
 
 
-if __name__ == '__main__':
-	global tee_goal
-	sub_tee_goal = rospy.Subscriber('/tee_goal', Pose, cb_tee_goal)
-	arm_group = movegroup_init()
-	rospy.init_node('robot_move')
-	robot_init = arm_group.get_current_pose().pose
-	print "============ Arm current pose: ", robot_init
-	print "click Enter to continue"
-	dummy_input = raw_input()
-	rate = rospy.Rate(10.0)
-	while not rospy.is_shutdown():
-		# tee_goal = plan_task_space_control(arm_group, robot_init, tee_goal)
-		arm_group.set_pose_target(tee_goal)
-		plan = arm_group.go(wait=True)
-		arm_group.stop()
-		arm_group.clear_pose_targets()
+def main():
+	try:
+		global tee_goal
+		sub_tee_goal = rospy.Subscriber('/tee_goal', Pose, cb_tee_goal)
+		arm_group = movegroup_init()
+		rospy.init_node('robot_move')
+		robot_init = arm_group.get_current_pose().pose
+		print "============ Arm current pose: ", robot_init
+		print "click Enter to continue"
+		dummy_input = raw_input()
+		rate = rospy.Rate(10.0)
+		while not rospy.is_shutdown():
+			cartesian_control_with_IMU(arm_group, robot_init, tee_goal)
+			# arm_group.set_pose_target(tee_goal)
+			# plan = arm_group.go(wait=True)
+			# arm_group.stop()
+			# arm_group.clear_pose_targets()
 
-		print "tee_goal:", tee_goal
-		rate.sleep()
+			print "tee_goal:", tee_goal
+			rate.sleep()
+	except KeyboardInterrupt:
+		moveit_commander.roscpp_shutdown()
+		rospy.signal_shutdown("KeyboardInterrupt")
+		raise
 
+
+if __name__ == '__main__': main()
