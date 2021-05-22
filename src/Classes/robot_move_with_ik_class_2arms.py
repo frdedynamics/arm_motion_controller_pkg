361ea4f59640904285dd9c5f79d5b8d962fe6e1d
gizem <gizemates@iyte.edu.tr>
2021-05-22 20:06:56 +0200

    motion hand colift init okay, robot init not

--- src/Classes/robot_move_with_ik_class_2arms.py
+++ src/Classes/robot_move_with_ik_class_2arms.py
@@ -145,22 +145,27 @@ class RobotCommander:
 		# self.robot_pose.orientation = kinematic.q_multiply(self.robot_init.orientation, kinematic.q_multiply(self.hand_init_orientation, self.motion_hand_pose.orientation))
 		self.robot_pose.orientation = self.robot_init.orientation
 
-		if not self.colift_flag:
-			self.robot_colift_init = self.robot_pose
-			self.motion_hand_colift_init = self.motion_hand_pose
+		self.motion_hand_colift_init = self.motion_hand_pose
+		print "here:", "motion_hand_colift_init:", self.motion_hand_colift_init.position
 
 
 	def cartesian_control_1_arm(self):	
 		self.motion_hand_colift_pos_ch.x = self.motion_hand_pose.position.x - self.motion_hand_colift_init.position.x
 		self.motion_hand_colift_pos_ch.y = self.motion_hand_pose.position.y - self.motion_hand_colift_init.position.y
 		self.motion_hand_colift_pos_ch.z = self.motion_hand_pose.position.z - self.motion_hand_colift_init.position.z
-		print "self.motion_hand_colift_init:", self.motion_hand_colift_init
+		print "self.robot_colift_init:", self.robot_colift_init
+		print "self.motion_hand_colift_pos_ch:", self.motion_hand_colift_pos_ch
 
 		corrected_motion_hand_pose = kinematic.q_rotate(self.human_to_robot_init_orientation, self.motion_hand_colift_pos_ch)
 		# print "corrected_motion_hand_pose:", corrected_motion_hand_pose
-		self.robot_pose.position.x = self.robot_colift_init.position.x + self.k * corrected_motion_hand_pose[0]
-		self.robot_pose.position.y = self.robot_colift_init.position.y + self.k * corrected_motion_hand_pose[1]
-		self.robot_pose.position.z = self.robot_colift_init.position.z + self.k * corrected_motion_hand_pose[2]
+		
+		self.robot_pose.position.x = self.robot_colift_init.position.x + self.k * self.motion_hand_colift_pos_ch.x
+		self.robot_pose.position.y = self.robot_colift_init.position.y + self.k * self.motion_hand_colift_pos_ch.y
+		self.robot_pose.position.z = self.robot_colift_init.position.z + self.k * self.motion_hand_colift_pos_ch.z
+
+		# self.robot_pose.position.x = self.robot_colift_init.position.x #+ self.k * corrected_motion_hand_pose[0]
+		# self.robot_pose.position.y = self.robot_colift_init.position.y #+ self.k * corrected_motion_hand_pose[1]
+		# self.robot_pose.position.z = self.robot_colift_init.position.z #+ self.k * corrected_motion_hand_pose[2]
 		# self.robot_pose.orientation = kinematic.q_multiply(self.robot_init.orientation, kinematic.q_multiply(self.hand_init_orientation, self.motion_hand_pose.orientation))
 		self.robot_pose.orientation = self.robot_colift_init.orientation
 
@@ -196,6 +201,7 @@ class RobotCommander:
 					else:
 						self.state == "CO-LIFT"
 						if not self.colift_flag:
+							self.robot_colift_init = self.robot_pose
 							self.colift_flag = True
 			elif(self.steering_hand_pose.orientation.w > 0.707 and self.steering_hand_pose.orientation.x < 0.707):
 				self.state = "IDLE"
